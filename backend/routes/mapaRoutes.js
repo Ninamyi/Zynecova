0f44d37ad5d2dabcccd5cabadc7ee09e9bd50a90
Ninamyi <duartenoemi2005@gmail.com>
2025-11-19 13:57:35 -0500

    Eliminar todo el contenido del proyecto

--- backend/routes/mapaRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getDatosCiudad } = require("../controllers/mapaController");
-
-router.get("/", getDatosCiudad);
-
-module.exports = router;
